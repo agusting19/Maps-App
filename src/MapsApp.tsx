50c2775b1f5788ccafc7c2bf17c08c69b6699d5d
Agustin Gomez <agusleongomez@gmail.com>
2022-10-04 00:32:30 -0300

    Context created

---
+++ src/MapsApp.tsx
@@ -0,0 +1,9 @@
+import { PlacesProvider } from "./context";
+
+export default function MapsApp() {
+  return (
+    <PlacesProvider>
+        <h1>Hello world again</h1>
+    </PlacesProvider>
+  )
+}
